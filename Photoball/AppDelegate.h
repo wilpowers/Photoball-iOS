2ac39537603ae5d6fc2d1e404561f65b46f99a61
gyrocade <wpowers@me.com>
2013-11-27 13:40:52 -0500

    Initial Commit

---
+++ Photoball/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photoball
+//
+//  Created by William Powers on 11/27/13.
+//  Copyright (c) 2013 Gyrocade, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photoball/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photoball
+//
+//  Created by William Powers on 11/27/13.
+//  Copyright (c) 2013 Gyrocade, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
